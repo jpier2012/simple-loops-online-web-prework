67cdc4b2b1ac501e74b9537f3efaaefa37c93a72
James Pier <jpier2012@gmail.com>
2019-04-01 14:09:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,6 +54,6 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for num in 1..number_of_times
     puts phrase
-    next num
+    num += 1
   end
 end
